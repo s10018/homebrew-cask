2acd3e2d3e1db8dfc1d221025e61c3e5282eeaf0
OMURA Mai <omura.mai.oz5@is.naist.jp>
2015-04-04 00:40:55 +0900

    fix checksum

--- Casks/font-migmix1p.rb
+++ Casks/font-migmix1p.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'font-migmix1p' do
   version '20130617'
-  sha256 '639bf85bca573d6d3be5487c8612dac88586d37c823ddb002584f59dcf7f3aa8'
+  sha256 '13d131ecc295823bace9e4d53a689ae26f21f18335a004c1a3d987a273467833'
 
   homepage 'http://mix-mplus-ipa.sourceforge.jp/migmix/'
   url "http://iij.dl.sourceforge.jp/mix-mplus-ipa/59021/migmix-1p-#{version}.zip"
