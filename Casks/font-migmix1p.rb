059918d717426c565f29177efb7e2944f10e18ff
OMURA Mai <omura.mai.oz5@is.naist.jp>
2015-04-04 00:37:39 +0900

    fix url

--- Casks/font-migmix1p.rb
+++ Casks/font-migmix1p.rb
@@ -3,7 +3,7 @@ cask :v1 => 'font-migmix1p' do
   sha256 '639bf85bca573d6d3be5487c8612dac88586d37c823ddb002584f59dcf7f3aa8'
 
   homepage 'http://mix-mplus-ipa.sourceforge.jp/migmix/'
-  url "http://iij.dl.sourceforge.jp/mix-mplus-ipa/59021/migmix-2p-#{version}.zip"
+  url "http://iij.dl.sourceforge.jp/mix-mplus-ipa/59021/migmix-1p-#{version}.zip"
 
   font 'migmix-1p-20130617/migmix-1p-bold.ttf'
   font 'migmix-1p-20130617/migmix-1p-regular.ttf'
