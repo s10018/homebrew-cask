7ec1c1d5bb751e9b420c972ae687110adaf4327d
OMURA Mai <omura.mai.oz5@is.naist.jp>
2016-01-16 19:10:58 +0900

    Add font-hanazono

---
+++ Casks/font-hanazono.rb
@@ -0,0 +1,10 @@
+cask :v1 => 'font-hanazono' do
+  version '20141012'
+  sha256 '06a7285dcc5c8a22aa3a1b319f965d9d336b84af61fabd5d9d5ba6fa2d861208'
+
+  url 'http://osdn.jp/frs/redir.php?m=iij&f=%2Fhanazono-font%2F62072%2Fhanazono-20141012.zip'
+  homepage 'http://fonts.jp/hanazono/'
+
+  font 'HanaMinA.ttf'
+  font 'HanaMinB.ttf'
+end
